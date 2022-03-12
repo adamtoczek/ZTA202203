93f2aad30510b956103259a8efca09b59ea7d45a
Adam Toczek <a.a.toczek@gmail.com>
2022-03-12 14:09:15 +0100

    foreach na tablicy

--- src/main/java/Listy.java
+++ src/main/java/Listy.java
@@ -4,9 +4,11 @@ import java.util.List;
 public class Listy {
     public static void main(String[] args) {
         List<Integer> liczby = new ArrayList<>();
+        int[] tab = new int[101];
 
-        for (int i = 0; i<100; i++) {
+        for (int i = 0; i<=100; i++) {
                 liczby.add(i);
+                tab[i] = i;
         }
 
         for (Integer v : liczby) {
@@ -16,10 +18,18 @@ public class Listy {
             }
         }
 
+        for (int i : tab) {
+            if (tab[i]%2 == 0)
+                tab[i] = i*2;
+        }
+
 
 
         for (Integer v : liczby)
             System.out.println(v);
+
+        for (int i : tab)
+            System.out.println(i);
     }
 
 }
